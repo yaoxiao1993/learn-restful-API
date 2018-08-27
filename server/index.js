ed544d7a63627017dfd627cde5b39489ff5747e9
yaoxiaoyu <yaoxiao0525@foxmail.com>
2018-08-27 21:55:38 +0800

    finished connect mongodb

--- server/index.js
+++ server/index.js
@@ -1,6 +1,27 @@
 //导入 express 功能模块，创建一个 express 应用实例名为 app
-const express = require('express')
+const express = require('express');
 const app = express();
+
+//引入mongoose数据库驱动
+const mongoose = require('mongoose');
+//设置连接位置
+mongoose.connect('mongodb://localhost:27017/movie-db',{useNewUrlParser:true});
+const db = mongoose.connection;
+//引入MovieSchema
+const Movie = require('../module/movie')
+//监听连接消息
+db.on('error', console.log);
+//手动连接一次，回调函数打印success
+db.once('open', ()=>{
+    let movie = new Movie({title: '齐天大圣'});
+    movie.save(function(err){
+        console.log("1!");
+        if(err)
+            console.log(err)   
+    })
+    console.log("success2!");
+});
+
 //app.get() 接口会响应 HTTP GET 请求，
 //当访问路径与 / 相匹配的时候，则执行上述代码，并通过 res.send() 接口向客户端发送 Hello world! 字符串。
 app.get('/',function(req, res){
